1f0988347a71a52d93379de2b25cbe71c4ef706c
Niranjan Reddy <niranjanreddynakkala@gmail.com>
2026-07-24 23:13:23 +0530

    debugging 3

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -159,9 +159,9 @@ USE_TZ = True
 # ------------------------------------------------------------------------------
 # Static & media files
 # ------------------------------------------------------------------------------
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
