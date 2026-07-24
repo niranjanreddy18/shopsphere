d32bac0685a3a98a6ccf559ebcd1a4a45d237334
Niranjan Reddy <niranjanreddynakkala@gmail.com>
2026-07-24 21:29:23 +0530

    changed urls.py to accept media urls in production

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
